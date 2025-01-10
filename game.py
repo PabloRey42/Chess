c4a4edfe48d4177b0f59336a14be15b3078bf67e
Pablo REY <61079549+PabloRey42@users.noreply.github.com>
2025-01-10 16:47:28 +0100

    first commit

---
+++ game.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+
+pygame.display.set_mode((400,400))
+
+clock = pygame.time.Clock()
+
+
+def main():
+    running = True
+    fps = 60
+    while running:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+pygame.quit()
---
+++ game/images/constants.py
@@ -0,0 +1,10 @@
+import pygame
+import os
+
+
+Width, Height = 760,760
+Rows, Cols = 8,8
+Square = Width//Rows
+
+
+Path =""
